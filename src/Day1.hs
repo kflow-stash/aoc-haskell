0e928f5ccf6d6be8f857e5e38e4c80d99388d17b
Kreg <kflow7@gmail.com>
2022-12-01 10:11:39 -0700

    reupload Day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -8,18 +8,18 @@ import Data.List.Split
 main = do  
     contents <- readFile "data/day1.txt"
     let elf_loads = map (map readInt . lines) (splitOn "\n\n" contents)
-        elf_sums = DL.sort $ map sum elf_loads
+        elf_sums = DL.sortBy (flip compare) $ map sum elf_loads
 
     --let lines_ = concat $ splitElfs $ DL.lines contents
     --    lines_str = map (wordsWhen (== ',')) (wordsWhen (==';') lines_)
     --    lines_int = map (map readInt) lines_str
     --    elf_sums = DL.sort $ map sum lines_int
 
-    print $ "largest load: " ++ show (last elf_sums)
+    print $ "largest load: " ++ show (head elf_sums)
 
-    print $ "top 3 loads: " ++ show (take 3 (reverse elf_sums))
+    print $ "top 3 loads: " ++ show (take 3 elf_sums)
 
-    print $ "sum of top 3 loads: " ++ show (sum (take 3 (reverse elf_sums)))
+    print $ "sum of top 3 loads: " ++ show (sum (take 3 elf_sums))
 
 
 splitElfs :: [String] -> [String]
