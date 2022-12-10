15b177510c630b88308236d81e44573fec47738d
Kreg Flowers <kflow7@gmail.com>
2022-12-09 23:06:27 -0700

    easy day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -2,49 +2,33 @@ module Day10 (main) where
 
 import System.IO 
 import AOCFuncs
-import Data.List (tails,nub)
+import Data.List (intercalate)
+import Data.List.Split
 
-moveHead :: [[Int]] -> [Int] -> [[Int]]
-moveHead [h0@[hy0,hx0], t0@[ty0,tx0]] [dy,dx] 
-    | isAdjacent [hy1, hx1] t0 = [[hy1,hx1], t0]
-    | otherwise = [[hy1,hx1], [ty0 + signum dy1,tx0 + signum dx1]]
-        where   hy1 = hy0 + dy
-                hx1 = hx0 + dx
-                dy1 = hy1 - ty0
-                dx1 = hx1 - tx0
+getSignal n cycles = (sum (take (n-2) cycles) + 1) * n
 
-isAdjacent :: [Int] -> [Int] -> Bool
-isAdjacent [hy1,hx1] [ty0,tx0]
-    | (abs (hx1 - tx0) > 1) || (abs (hy1 - ty0) > 1) = False
-    | otherwise = True
-
-parseIn :: [String] -> [[Int]]
-parseIn [rel,steps] = replicate (readInt steps) (vectorize rel)
-    where   vectorize "R" = [0,1]
-            vectorize "L" = [0,-1]
-            vectorize "U" = [-1,0]
-            vectorize "D" = [1,0]
-
-removeSucc :: [[Int]] -> [[Int]]
-removeSucc [] = []
-removeSucc [x] = [x]
-removeSucc (x1:x2:xs)
-            | x1 == x2 = removeSucc (x2:xs)
-            | otherwise = x1 : removeSucc (x2:xs)
-
-getTailMoves :: [[Int]] -> [[Int]]
-getTailMoves [] = []
-getTailMoves [x] = []
-getTailMoves (x1:x2:xs) = [head x2 - head x1,last x2 - last x1]:getTailMoves (x2:xs)
-
-applyMoves p0 moves = foldl (\acc x -> moveHead (head acc) x : acc) p0 vectors
-    where vectors = getTailMoves $ removeSucc $ reverse $ map last moves
+getSignalVal n cycles = if (n_mod>=signal_val) && (n_mod<=(signal_val + 2)) then '#' else '.'
+    where   n_mod = if n `mod` 40 == 0 then 40 else n `mod` 40
+            signal_val = sum (take (n-2) cycles) + 1
 
 
 main = do  
     contents <- readFile "data/day10.txt"
     let input_ = map words $ lines contents
+        cycles = concat [if length xs > 1 then [readInt $ last xs,0] else [0]| xs <- input_]
 
+        x_20 = getSignal 20 cycles
+        x_60 = getSignal 60 cycles
+        x_100 = getSignal 100 cycles
+        x_140 = getSignal 140 cycles
+        x_180 = getSignal 180 cycles
+        x_220 = getSignal 220 cycles
 
+        part1 = sum [x_20,x_60,x_100,x_140,x_180,x_220]
 
-    print input_
+        signals = map (`getSignalVal` cycles) [1..240]
+        signal_rows = intercalate "\n" $ chunksOf 40 signals
+
+    writeFile "data/day10_out.txt" signal_rows
+
+    print part1
